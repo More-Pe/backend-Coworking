07b17dfef7956fab4b88354aaee47478a1ab5895
More-Pe <peraltamorena.va@gmail.com>
2024-09-20 13:00:46 +0200

    chore: configurate server

--- dist/server.js
+++ dist/server.js
@@ -1,1 +1,11 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const db_1 = require("./database/db");
+db_1.AppDataSource.initialize()
+    .then(() => {
+    console.log('Database connected');
+})
+    .catch((error) => {
+    console.log(error);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
-import 'dotenv/config';
-import { AppDataSource } from './database/db';
+import dotenv from 'dotenv';
+import { AppDataSource } from './database/db'
 import express from 'express';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:4000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+const PORT = process.env.PORT || 4000;
 
 AppDataSource.initialize()
 	.then(() => {
 		console.log('Database connected');
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		});
 	})
 	.catch((error) => {
 		console.log(error);
