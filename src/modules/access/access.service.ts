d985e6e3296c20fed7b13b2e819757d19a0a9693
More-Pe <peraltamorena.va@gmail.com>
2024-09-23 22:25:41 +0200

    refactor: access service

--- src/modules/access/access.service.ts
+++ src/modules/access/access.service.ts
@@ -6,50 +6,37 @@ import { IsNull } from 'typeorm';
 export class AccessService {
     
     public static async registerEntry(person_id: number, room_id: number): Promise<Access> {
-        // Verificar si la sala existe
         const room = await Room.findOne({ where: { room_id } });
         if (!room) {
             throw new Error('Room not found');
         }
     
-        // Verificar si la persona existe
         const person = await Person.findOne({ where: { person_id } });
         if (!person) {
             throw new Error('Person not found');
         }
-    
-        // Verificar si la persona ya tiene un acceso activo en cualquier sala
+
         const existingActiveAccess = await Access.findOne({
             where: {
                 person_id: person_id,
                 status: Status.Entry,
-                exit_time: IsNull(), // Acceso activo sin salida
+                exit_time: IsNull(),
             },
         });
     
-        console.log(`Existing active access for person ${person_id}:`, existingActiveAccess);
-    
-        // Si la persona ya tiene un acceso activo
         if (existingActiveAccess) {
-            // Verificar si la entrada activa es en la misma sala
             if (existingActiveAccess.room_id === room_id) {
-                console.log(`Person ${person_id} is trying to re-enter the same room: ${room_id}`);
                 throw new Error('You have already entered this room and haven’t registered an exit.');
             } else {
-                console.log(`Person ${person_id} is trying to enter a new room: ${room_id} while active in room: ${existingActiveAccess.room_id}`);
                 throw new Error('You already have an active entry in another room.');
             }
         }
     
-        // Comprobar la capacidad de la sala
         const currentPeople = await AccessService.getCurrentPeopleInRoom(room_id);
-        console.log(`Current people in room ${room_id}: ${currentPeople.length}, Capacity: ${room.capacity}`);
-    
-        if (currentPeople.length >= room.capacity) {
+        if (currentPeople.people.length >= room.capacity) {
             throw new Error('The room has reached its maximum capacity');
         }
     
-        // Crear nuevo acceso
         const access = Access.create({
             person_id: person.person_id,
             room_id: room.room_id,
@@ -57,14 +44,9 @@ export class AccessService {
             status: Status.Entry,
         });
     
-        console.log('Registering new entry:', access);
-    
-        // Guardar el nuevo acceso
         return await access.save();
     }
 
-
-    // Método para registrar una salida
     public static async registerExit(person_id: number, room_id: number): Promise<Access> {
         const room = await Room.findOne({ where: { room_id } });
         if (!room) {
@@ -93,13 +75,12 @@ export class AccessService {
         return await access.save();
     }
 
-    // Método para obtener las personas actuales en una sala
-    public static async getCurrentPeopleInRoom(room_id: number): Promise<Person[]> {
+    public static async getCurrentPeopleInRoom(room_id: number): Promise<{ people: Omit<Person, 'password'>[], count: number }> {
         const room = await Room.findOne({ where: { room_id } });
         if (!room) {
             throw new Error('Room not found');
         }
-
+    
         const accesses = await Access.find({
             where: {
                 room: room,
@@ -108,10 +89,14 @@ export class AccessService {
             },
             relations: ['person'],
         });
+    
+        const people = accesses.map(access => {
+            const { password, ...personData } = access.person;
+            return personData as Omit<Person, "password">;
+        });
 
-        const people = accesses.map(access => access.person);
-
-        return people;
+        return { count: people.length, people };
     }
 }
+
 
